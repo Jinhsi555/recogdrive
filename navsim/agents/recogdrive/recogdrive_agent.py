fix val loss computation

navsim/agents/recogdrive/recogdrive_agent.py
@@ -384,7 +384,7 @@ class ReCogDriveAgent(AbstractAgent):
             action_inputs = BatchFeature(data={"state": input_state.to(model_dtype), "his_traj": history_trajectory_reshaped.to(model_dtype), "status_feature": status_feature.to(model_dtype), "action": targets["trajectory"].to(model_dtype)})
             return self.action_head.forward_grpo(last_hidden_state, action_inputs, tokens_list)
         else: 
-            action_inputs = BatchFeature({"state": input_state.to(model_dtype), "his_traj": history_trajectory_reshaped.to(model_dtype), "status_feature": status_feature.to(model_dtype)})
+            # action_inputs = BatchFeature({"state": input_state.to(model_dtype), "his_traj": history_trajectory_reshaped.to(model_dtype), "status_feature": status_feature.to(model_dtype)})
             return self.action_head.get_action(last_hidden_state.to(model_dtype), action_inputs)
 
     def compute_trajectory(self, agent_input: AgentInput) -> Trajectory:
@@ -429,9 +429,9 @@ class ReCogDriveAgent(AbstractAgent):
         if self.training and self.grpo:
             return predictions
         elif self.training:
-            return predictions.loss
+            return predictions.action_loss, predictions.alignment_loss
         else:
-            return torch.nn.functional.l1_loss(predictions["pred_traj"], targets["trajectory"])
+            return torch.nn.functional.l1_loss(predictions["pred_traj"], targets["trajectory"]), predictions.alignment_loss
 
     def get_optimizers(self) -> Union[Optimizer, Dict[str, LRScheduler]]:
         """

navsim/agents/recogdrive/recogdrive_diffusion_planner.py
@@ -509,7 +509,7 @@ class ReCogDriveDiffusionPlanner(nn.Module):
             
             model_output = self.model(fused_input, vl_embeds, ego_status_features, t_discrete)
             pred_noise = self.action_decoder(model_output)
-            loss = F.mse_loss(pred_noise, noise, reduction='mean')
+            action_loss = F.mse_loss(pred_noise, noise, reduction='mean')
         
         if action_input.get("geometry_feature") is not None:
             alignment_feature = action_input.get("alignment_feature")
@@ -521,11 +521,11 @@ class ReCogDriveDiffusionPlanner(nn.Module):
             geometry_feature = F.interpolate(geometry_feature, size=(34, 60), mode='bilinear', align_corners=True).permute(0, 2, 3, 1).view(B*N, -1, C)
         
             # compute alignment loss
-            align_loss = self.compute_alignment_loss(geometry_feature, alignment_feature)
-            loss += 0.5 * align_loss
-            
-        return BatchFeature(data={"loss": loss})
+            alignment_loss = self.compute_alignment_loss(geometry_feature, alignment_feature)
+            return BatchFeature(data={"action_loss": action_loss, "alignment_loss": alignment_loss})
 
+        return BatchFeature(data={"action_loss": action_loss, "alignment_loss": None})
+        
     def compute_alignment_loss(self, geometry_feature: torch.Tensor, alignment_feature: torch.Tensor) -> torch.Tensor:
         align_loss = 0
         bsz = alignment_feature.shape[0]
@@ -663,7 +663,20 @@ class ReCogDriveDiffusionPlanner(nn.Module):
 
         final_actions = self.denorm_odo(current_actions)
 
-        return BatchFeature(data={"pred_traj": final_actions})
+        if action_input.get("geometry_feature") is not None:
+            alignment_feature = action_input.get("alignment_feature")
+            geometry_feature = action_input.get("geometry_feature")
+            B, N, H, W, C = geometry_feature.shape
+            geometry_feature = geometry_feature.permute(0, 1, 4, 2, 3).view(B*N, C, H, W)
+        
+            # interpolate the geometry feature map to match the size of alignment feature map
+            geometry_feature = F.interpolate(geometry_feature, size=(34, 60), mode='bilinear', align_corners=True).permute(0, 2, 3, 1).view(B*N, -1, C)
+        
+            # compute alignment loss
+            alignment_loss = self.compute_alignment_loss(geometry_feature, alignment_feature)
+
+        return BatchFeature(data={"pred_traj": final_actions,
+                                  "alignment_loss": alignment_loss})
 
     def sample_chain(
         self,

navsim/planning/script/run_training_recogdrive_debug.py
@@ -7,7 +7,6 @@ from hydra.utils import instantiate
 from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
-from pytorch_lightning.strategies import DDPStrategy
 import torch.distributed as dist
 from navsim.agents.abstract_agent import AbstractAgent
 from navsim.common.dataclasses import SceneFilter
@@ -17,6 +16,8 @@ from navsim.planning.training.agent_lightning_module import AgentLightningModule
 import torch
 import torch.nn.utils.rnn as rnn_utils
 from typing import List, Dict
+from callback import EMA, EMAModelCheckpoint
+import pytorch_lightning.callbacks as plc
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +25,34 @@ CONFIG_PATH = "config/training"
 CONFIG_NAME = "default_training"
 
 
+def load_callbacks():
+    callbacks = []
+    
+    use_ema = True
+    if use_ema:
+        callbacks.append(EMAModelCheckpoint(
+            monitor='val/loss_epoch',
+            mode='min',
+            save_last=True,
+            save_on_train_epoch_end=True,
+            save_top_k=10,
+            every_n_epochs=1
+        ))
+        callbacks.append(EMA(decay=0.999))
+    else:
+        callbacks.append(plc.ModelCheckpoint(
+            monitor='val/loss_epoch',
+            mode='min',
+            save_last=True,
+            save_on_train_epoch_end=True,
+            save_top_k=5,
+            every_n_epochs=1
+        ))
+
+    callbacks.append(plc.LearningRateMonitor(logging_interval='epoch'))
+
+
+    return callbacks
 
 
 def custom_collate_fn(
@@ -145,7 +174,7 @@ def main(cfg: DictConfig) -> None:
 
     logger.info("Building Agent")
     agent: AbstractAgent = instantiate(cfg.agent)
-    agent.initialize()
+    # agent.initialize()
 
     logger.info("Building Lightning Module")
     lightning_module = AgentLightningModule(
@@ -164,13 +193,13 @@ def main(cfg: DictConfig) -> None:
             cache_path=cfg.cache_path,
             feature_builders=agent.get_feature_builders(),
             target_builders=agent.get_target_builders(),
-            log_names=cfg.train_logs[:2],
+            log_names=cfg.train_logs[:1],
         )
         val_data = CacheOnlyDataset(
             cache_path=cfg.cache_path,
             feature_builders=agent.get_feature_builders(),
             target_builders=agent.get_target_builders(),
-            log_names=cfg.val_logs[:20],
+            log_names=cfg.val_logs[:50],
         )
     else:
         logger.info("Building SceneLoader")
@@ -183,11 +212,7 @@ def main(cfg: DictConfig) -> None:
     logger.info("Num validation samples: %d", len(val_data))
 
     logger.info("Building Trainer")
-    strategy = DDPStrategy(
-        find_unused_parameters=False,
-        timeout=3600,
-    )
-    trainer = pl.Trainer(**cfg.trainer.params, strategy=strategy, callbacks=[pl.callbacks.ModelCheckpoint(monitor="val/loss_epoch",mode='min', save_top_k=5,every_n_epochs=1)])
+    trainer = pl.Trainer(**cfg.trainer.params, callbacks=load_callbacks())
 
     logger.info("Starting Training")
     trainer.fit(